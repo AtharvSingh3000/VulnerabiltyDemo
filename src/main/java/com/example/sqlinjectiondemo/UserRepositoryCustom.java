a91a4d1ae7be70ac6947f3264f0b036c987ac359
AtharvSingh3000 <nitishsingh30012000@gmail.com>
2026-05-21 10:42:13 +0530

    Removed old duplicate merged files

--- src/main/java/com/example/sqlinjectiondemo/UserRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package com.example.sqlinjectiondemo;
-
-public interface UserRepositoryCustom {
-    User findUserByUsernameAndPassword(String username, String password);
-    User findUserByUsernameAndPasswordSecure(String username, String password);
-}
